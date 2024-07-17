f54321f94b6cfe47d56eb2fef11f852d35051587
NiBa97 <niba97@icloud.com>
2024-07-17 09:46:42 +0200

    feat: Add User menu

--- src/components/app-navbar.tsx
+++ src/components/app-navbar.tsx
@@ -1,6 +1,7 @@
-import { Box, Text, Flex, Link, Button } from "@chakra-ui/react";
+import { Box, Text, Flex, Link, Button, MenuList, MenuItem, MenuButton, Avatar, Menu } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import { FaSignOutAlt } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
 
 export default function AppNavbar() {
   const { data: session } = useSession();
@@ -25,6 +26,26 @@ export default function AppNavbar() {
         <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
           <Button leftIcon={<FaSignOutAlt />}>Sign out</Button>
         </Link>
+      </Box>
+      <Box>
+        <Menu>
+          <MenuButton as={Box} cursor="pointer">
+            <Flex alignItems={"center"} gap={2}>
+              <Avatar size="sm" name={session.user?.name ?? ""} src={session.user?.image ?? ""} />
+              <Text fontSize={"md"} fontWeight={"bold"}>
+                {session.user?.name ?? ""}
+              </Text>
+              <FaChevronDown />
+            </Flex>
+          </MenuButton>
+          <MenuList>
+            <MenuItem as={Link} href="/api/auth/signout">
+              <Button leftIcon={<FaSignOutAlt />} variant="link">
+                Sign out
+              </Button>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Box>
     </Flex>
   );
