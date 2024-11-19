57dfa9328936e00bf110e74e4a26f0536134c564
NiBa97 <niba97@icloud.com>
2024-11-19 21:02:52 +0100

    refactor: Move chakra provider to global providers

--- src/app/webapp/layout.tsx
+++ src/app/webapp/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Box, ChakraProvider, Flex, createMultiStyleConfigHelpers, extendTheme } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { Resizable, type ResizeCallbackData } from "react-resizable";
 import { TaskProvider } from "../../contexts/task-context";
 import TaskMenu from "~/components/task-menu";
@@ -9,95 +9,6 @@ import React from "react";
 import { ResizeHandle } from "~/components/resize-handle";
 import AppNavbar from "~/components/app-navbar";
 import { AttachmentProvider } from "~/contexts/attachment-context";
-
-// This function creates a set of function that helps us create multipart component styles.
-const helpers = createMultiStyleConfigHelpers(["menu", "item"]);
-
-const Menu = helpers.defineMultiStyleConfig({
-  baseStyle: {
-    menu: {
-      boxShadow: "lg",
-      rounded: "lg",
-      flexDirection: "column",
-      py: "2",
-    },
-    item: {
-      fontWeight: "medium",
-      lineHeight: "normal",
-      color: "gray.600",
-    },
-  },
-  sizes: {
-    sm: {
-      item: {
-        fontSize: "0.75rem",
-        px: 2,
-        py: 1,
-      },
-    },
-    md: {
-      item: {
-        fontSize: "0.875rem",
-        px: 3,
-        py: 2,
-      },
-    },
-  },
-  variants: {
-    bold: {
-      item: {
-        fontWeight: "bold",
-      },
-      menu: {
-        boxShadow: "xl",
-      },
-    },
-    colorful: {
-      item: {
-        color: "orange.600",
-      },
-      menu: {
-        bg: "orange.100",
-      },
-    },
-  },
-  defaultProps: {
-    size: "md",
-  },
-});
-const theme = extendTheme({
-  colors: {
-    brand: {
-      1: "#222831",
-      2: "#393E46",
-      3: "#00ADB5",
-      4: "#EEEEEE",
-    },
-  },
-  components: {
-    Button: {
-      baseStyle: {
-        bg: "brand.2",
-        color: "brand.4",
-        border: "none",
-        _hover: {
-          bg: "brand.4",
-        },
-      },
-      variants: {
-        solid: {
-          bg: "brand.2",
-          color: "brand.4",
-          border: "none",
-          _hover: {
-            bg: "brand.3",
-          },
-        },
-      },
-    },
-  },
-  Menu,
-});
 
 export default function Layout({ calendar, children }: { calendar: React.ReactNode; children: React.ReactNode }) {
   const savedWidth = localStorage.getItem("width") ?? "0.5";
@@ -122,42 +33,40 @@ export default function Layout({ calendar, children }: { calendar: React.ReactNo
   };
 
   return (
-    <ChakraProvider theme={theme}>
-      <TaskProvider>
-        <AttachmentProvider>
-          <AppNavbar></AppNavbar>
-          <Flex
+    <TaskProvider>
+      <AttachmentProvider>
+        <AppNavbar></AppNavbar>
+        <Flex
+          maxHeight={"calc(100vh - 50px)"}
+          width={"100vw"}
+          height={"calc(100vh - 50px)"}
+          bg={"brand.1"}
+          color={"brand.4"}
+          gap={2}
+        >
+          <Resizable
+            axis="x"
+            width={width}
+            onResize={onWidthResize}
+            resizeHandles={["e"]}
+            className={"resize-x"}
+            handle={<ResizeHandle handleAxis={"x"} innerRef={null} />}
+          >
+            <Box width={width}>{children}</Box>
+          </Resizable>
+          <Box
+            width={`calc(100vw - ${width}px)`}
             maxHeight={"calc(100vh - 50px)"}
-            width={"100vw"}
-            height={"calc(100vh - 50px)"}
             bg={"brand.1"}
             color={"brand.4"}
-            gap={2}
+            className="test123"
           >
-            <Resizable
-              axis="x"
-              width={width}
-              onResize={onWidthResize}
-              resizeHandles={["e"]}
-              className={"resize-x"}
-              handle={<ResizeHandle handleAxis={"x"} innerRef={null} />}
-            >
-              <Box width={width}>{children}</Box>
-            </Resizable>
-            <Box
-              width={`calc(100vw - ${width}px)`}
-              maxHeight={"calc(100vh - 50px)"}
-              bg={"brand.1"}
-              color={"brand.4"}
-              className="test123"
-            >
-              {calendar}
-            </Box>
-            <TaskMenu />
-            <TaskEditModal />
-          </Flex>
-        </AttachmentProvider>
-      </TaskProvider>
-    </ChakraProvider>
+            {calendar}
+          </Box>
+          <TaskMenu />
+          <TaskEditModal />
+        </Flex>
+      </AttachmentProvider>
+    </TaskProvider>
   );
 }
--- src/contexts/providers.tsx
+++ src/contexts/providers.tsx
@@ -2,6 +2,99 @@
 import { SessionProvider } from "next-auth/react";
 import { type Session } from "next-auth";
 import React from "react";
+import { ChakraProvider, createMultiStyleConfigHelpers, extendTheme } from "@chakra-ui/react";
+// This function creates a set of function that helps us create multipart component styles.
+const helpers = createMultiStyleConfigHelpers(["menu", "item"]);
+
+const Menu = helpers.defineMultiStyleConfig({
+  baseStyle: {
+    menu: {
+      boxShadow: "lg",
+      rounded: "lg",
+      flexDirection: "column",
+      py: "2",
+    },
+    item: {
+      fontWeight: "medium",
+      lineHeight: "normal",
+      color: "gray.600",
+    },
+  },
+  sizes: {
+    sm: {
+      item: {
+        fontSize: "0.75rem",
+        px: 2,
+        py: 1,
+      },
+    },
+    md: {
+      item: {
+        fontSize: "0.875rem",
+        px: 3,
+        py: 2,
+      },
+    },
+  },
+  variants: {
+    bold: {
+      item: {
+        fontWeight: "bold",
+      },
+      menu: {
+        boxShadow: "xl",
+      },
+    },
+    colorful: {
+      item: {
+        color: "orange.600",
+      },
+      menu: {
+        bg: "orange.100",
+      },
+    },
+  },
+  defaultProps: {
+    size: "md",
+  },
+});
+const theme = extendTheme({
+  colors: {
+    brand: {
+      1: "#222831",
+      2: "#393E46",
+      3: "#00ADB5",
+      4: "#EEEEEE",
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        bg: "brand.2",
+        color: "brand.4",
+        border: "none",
+        _hover: {
+          bg: "brand.4",
+        },
+      },
+      variants: {
+        solid: {
+          bg: "brand.2",
+          color: "brand.4",
+          border: "none",
+          _hover: {
+            bg: "brand.3",
+          },
+        },
+      },
+    },
+  },
+  Menu,
+});
 export function Providers(props: { children: React.ReactNode; serverSession: Session | null }) {
-  return <SessionProvider session={props.serverSession}>{props.children}</SessionProvider>;
+  return (
+    <SessionProvider session={props.serverSession}>
+      <ChakraProvider theme={theme}>{props.children}</ChakraProvider>
+    </SessionProvider>
+  );
 }
