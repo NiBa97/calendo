f6ee96557976e560ad631a966d6863c4d283b416
NiBa97 <niba97@icloud.com>
2025-02-13 20:19:01 +0100

    fix: Improve styling due to chakra version update

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,3 +1,3 @@
 export default function Notes() {
-  return <>Empty</>;
+  return <></>;
 }
--- src/pocketbaseUtils.tsx
+++ src/pocketbaseUtils.tsx
@@ -1,10 +1,10 @@
 import PocketBase from "pocketbase";
-import { useNavigate } from "react-router-dom";
 const pb = new PocketBase("http://192.168.0.84:8080/");
 export function checkIfLoggedIn(): boolean {
   return pb.authStore.isValid;
 }
 export function logout() {
+  alert("logged out");
   pb.authStore.clear();
 }
 export function getPb() {
