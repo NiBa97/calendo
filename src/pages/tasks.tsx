bdbb938da5e5899924b047594bed1594c250b467
NiBa97 <niba97@icloud.com>
2025-03-02 22:26:30 +0100

    design: Task Page

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,25 +1,50 @@
-import { Box, Flex, useDisclosure } from "@chakra-ui/react";
+import { Box, Flex, Separator, useDisclosure } from "@chakra-ui/react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import ListTasks from "../components/task-list";
 import CreateTask from "../components/create-task";
 import MainCalendar from "../components/calendar/main-calendar";
 import TaskContextMenu from "../components/task-contextmenu";
+
 export default function Tasks() {
   const { open: isTaskListOpen, onToggle: toggleTasklist } = useDisclosure({ defaultOpen: true });
 
   return (
     <Flex direction="column" flex={1}>
-      {/* Resizable Columns */}
       <PanelGroup direction="horizontal" style={{ height: "100%" }}>
         {isTaskListOpen && (
           <>
             <Panel defaultSize={50} minSize={20}>
-              <CreateTask></CreateTask>
-              <ListTasks></ListTasks>
+              <Box p={2}>
+                <CreateTask></CreateTask>
+                <Separator my={2}></Separator>
+                <ListTasks></ListTasks>
+              </Box>
             </Panel>
 
-            <PanelResizeHandle style={{ width: "8px", cursor: "col-resize" }}>
-              <Box w="full" h="full" bg="gray.100" _hover={{ bg: "blue.100" }} />
+            <PanelResizeHandle style={{ width: "12px", cursor: "col-resize" }}>
+              <Flex
+                w="full"
+                h="full"
+                justifyContent="center"
+                alignItems="center"
+                position="relative"
+                _before={{
+                  content: '""',
+                  position: "absolute",
+                  height: "60px",
+                  width: "4px",
+                  borderRadius: "full",
+                  bg: "gray.300",
+                  transition: "all 0.2s ease",
+                }}
+                _hover={{
+                  _before: {
+                    bg: "brand.3",
+                    height: "80px",
+                    boxShadow: "0 0 8px rgba(0, 173, 181, 0.5)",
+                  },
+                }}
+              />
             </PanelResizeHandle>
           </>
         )}
